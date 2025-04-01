7dee43e04f73040df789ccf4e138bfeb93de9d98
Ares <143079543+0xares0@users.noreply.github.com>
2025-04-01 19:28:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,41 +6,38 @@ def number_guessing_game():
 
     # Init the game
     if "game active" not in st.session_state:
+        st.session_state.guesses = 0
         st.session_state.game_active = False
         st.session_state.lives_left = 0
-        st.session_state.max_lives = 5
-        st.session_state.guess = None
+        st.session_state.max_lives = 0
     
     # Game start
     if st.button("Game Start"):
         st.session_state.game_active = True
         st.session_state.max_lives = 5
         st.session_state.lives_left = st.session_state.max_lives
-        st.session_state.secret_number = random.randint(1, 5)
-        st.session_state.guesses = 0
+        st.session_state.secret_number = random.randint(1, 500)
     
     st.write(f"{st.session_state.max_lives}")
-    st.write(f"{st.session_state.guesses}")
-
-    st.session_state.guess = st.number_input("Guess the number from 1 to 5")
+    st.write(f"{st.session_state.lives_left}")
+    
+    guess = st.number_input("Guess the number")
     
     # Guessing the number
     if st.session_state.lives_left > 0:
         if st.button("Submit Number"):
-            if st.session_state.guess == st.session_state.secret_number:
+            if guess == st.session_state.secret_number:
                 st.write("Congratulations, you got the number")
                 st.session_state.guesses += 1
                 st.session_state.game_active = False
             
             else:
+                st.write("Wrong number. Guess again")
                 st.session_state.guesses += 1
                 st.session_state.lives_left -= 1
-    
-                if st.session_state.lives_left > 0:
-                    st.write(f"Wrong number. You have {st.session_state.lives_left} lives left")
-                else:
-                    st.error(f"Game over. Try Again")
-                    st.session_state.game_active = False
+                st.error(f"You have {st.session_state.lives_left} lives left")
+    else:
+        st.error("Game Over. Try again")
     
 
 if __name__ == "__main__":
