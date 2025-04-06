9f0ebff1b3465a383d309abbd5bf2c57c086f792
Ares <143079543+0xares0@users.noreply.github.com>
2025-04-06 22:26:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,32 @@ def number_guessing_game():
     st.title("Number guessing game")
 
     # Init the game
-    if "game active" not in st.session_state:
-        st.session_state.game_active = False
-        st.session_state.max_lives = None
-        st.session_state.guesses = 0
+    def init_the_game():
+        if "game active" not in st.session_state:
+            st.session_state.game_active = False
+            st.session_state.max_lives = None
+            st.session_state.guesses = 0
         
     
     # Game start
-    if st.button("Game Start"):
-        if not st.session_state.game_active:
-            st.session_state.guesses = 0
-            st.session_state.game_active = True
-            st.session_state.max_lives = 5
-            st.session_state.lives_left = st.session_state.max_lives
-            st.session_state.secret_number = random.randint(1, 50)
+    def start_game():
+        if st.button("Game Start"):
+            if not st.session_state.game_active:
+                st.session_state.guesses = 0
+                st.session_state.game_active = True
+                st.session_state.max_lives = 5
+                st.session_state.lives_left = st.session_state.max_lives
+                st.session_state.secret_number = random.randint(1, 50)
             
     
     st.write(f"{st.session_state.max_lives} max lives")
     st.write(f"{st.session_state.guesses} guesses")
     st.write(f"{st.session_state.game_active}")
     
-    user_guess = st.number_input("Guess the number between 1 and 50", step=1)
+    user_guess = st.number_input("Guess the number between 1 and 50", step=1, min=1, max=50)
     
     # Guessing the number
-    if st.button("Submit Number"):
+    if st.button("Submit Number", on_click=start_game):
         if st.session_state.lives_left > 0:
             guess = int(user_guess)
             st.session_state.guesses += 1
