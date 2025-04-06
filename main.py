4daa73dca59cebdd06a8c72f2b4771cb4f60bd6d
0xares <0xares0@gmail.com>
2025-04-06 16:30:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,24 @@ def number_guessing_game():
             st.session_state.max_lives = 5
             st.session_state.lives_left = st.session_state.max_lives
             st.session_state.secret_number = random.randint(1, 50)
-            st.session_state.guess = None
     
-    st.write(f"{st.session_state.max_lives}")
+    st.write(f"{st.session_state.max_lives} max lives")
     st.write(f"{st.session_state.guesses} guesses")
     
     user_guess = st.number_input("Guess the number between 1 and 50", step=1)
     
     # Guessing the number
-    if st.button("Submit Number"):
-        if st.session_state.game_active and st.session_state.lives_left > 0:
-            st.session_state.guess = int(user_guess)
+    if st.button("Submit Number") and st.session_state.game_active:
+        if st.session_state.lives_left > 0:
+            guess = int(user_guess)
+            st.session_state.guesses += 1
                     
-            if st.session_state.guess == st.session_state.secret_number:
+            if guess == st.session_state.secret_number:
                 st.success("Congratulations, you got the number")
                 st.session_state.game_active = False
             
             else:
                 
-                st.session_state.guesses += 1
                 st.session_state.lives_left -= 1
             
                 if st.session_state.lives_left > 0:
@@ -46,7 +45,7 @@ def number_guessing_game():
                 else:
                     st.error(f"Game Over. Try again. The correct answer was {st.session_state.secret_number}")
                     st.session_state.game_active = False
-        else:
+    else:
             st.warning("Please click on Game Start")
     
 
